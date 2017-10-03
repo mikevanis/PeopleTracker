1beacdf80a7a566e0f9403fc28352eed0a52027e
Mike Vanis <hello@mikevanis.com>
2017-10-03 17:57:24 +0100

    first commit

---
+++ PeopleTracker.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+from imutils.video import WebcamVideoStream
+import imutils
+
+vs = WebcamVideoStream(src=0).start()
+avg = None
+
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if avg is None:
+        avg = gray.copy().astype("float")
+        continue
+
+    cv2.accumulateWeighted(gray, avg, 0.1)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        if cv2.contourArea(c) < 4000:
+            print("Tiny")
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
+
+    cv2.imshow("Diff", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
